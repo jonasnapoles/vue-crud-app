10b679298f3fbe73b41d03fee92ac98c35adede0
Jonas <jnoliveira@pdmfc.com>
2022-03-19 23:00:31 +0000

    first commit

---
+++ js/01.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Working with Vue :)',
+         fruits: [
+            // 'Apple', 'Watermelon', 'Pineapple'
+        //object
+        {name:'Apple', amount: 200  },
+        {name:'Watermelon', amount: 900  },
+        {name:'Pineapple', amount: 0  },
+        
+        ]
+        
+        
+    }
+})
